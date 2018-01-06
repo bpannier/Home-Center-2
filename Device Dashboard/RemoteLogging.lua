de5b8b71a8f4c530f2c33f13d2b8e74d34207942
Ben Pannier <github@ka.ro>
2018-01-06 22:53:53 +0100

    documentation

--- Device Dashboard/RemoteLogging.lua
+++ Device Dashboard/RemoteLogging.lua
@@ -5,7 +5,17 @@
 BenStatus
 --]]
 
+-- An example how to do remote logging. 
+-- Be careful not to use it in too many scenes which dumps simultaneously as
+-- otherwise the maximum amount of allowed instances of the remote logging scene
+-- will be reached and you will loose data.
+--
+-- by Benjamin Pannier <github@ka.ro>
+-- latest version: https://github.com/bpannier/Home-Center-2/tree/master/Device%20Dashboard
+
 local debugLevel = 3
+
+local sceneIDofDumpScript = 4
 
 local logDeviceClass = "logging"
 local logDeviceName = "dummy"
@@ -13,6 +23,7 @@ local logDeviceName = "dummy"
 local logCache = {}
 
 ------------------------------------------------------------------------------
+-- This log functions caches all log entries, use logFlush to send them remote.
 local function log(level, str)
   if debugLevel >= level then
     fibaro:debug(str);
@@ -29,24 +40,29 @@ local function log(level, str)
 end
 
 ------------------------------------------------------------------------------
+-- Dumps all cached log entries to the remote script/scene
 local function logFlush()
   
   local counter = 0
   local sendData = {}
+  
   for _, v in ipairs(logCache) do
+    -- encode via json the content of all log entries
     encoded = string.sub(json.encode({ value= '"' .. v .. '"' }),2,-2) 
     table.insert(sendData, { deviceClass = logDeviceClass, deviceName = logDeviceName, values = encoded})
     counter = counter + 1
   end
   
   if counter > 0 then
-    fibaro:startScene(4, sendData)
+    -- send all log entries 
+    fibaro:startScene(sceneIDofDumpScript, sendData)
   end
   
   logCache = {}
 end
 
 ------------------------------------------------------------------------------
+-- Example on how to do remote logging
 
 log(1, "test 1")
 log(1, "test 2")
